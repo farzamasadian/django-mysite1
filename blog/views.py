d4e7542935020095d43e3dac97b398444c458d5d
Farzam Asadian <farzamasadian@gmail.com>
2025-04-04 17:06:44 +0330

    finished up ordering posts and published date.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+
+    # Update status of posts that have been published
+    for post in posts:
+        if post.published_date <= timezone.now() and post.status != 1:  
+            post.status = 1 
+            post.save(update_fields=['status'])
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(Post, pk=pid, status=1, published_date__lte=timezone.now())
+
+    # Increase counted_view
+    post.counted_views += 1
+    post.save(update_fields=['counted_views'])
+
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
 
 def test(request, pid):
     # post = Post.objects.get(id=pid)
