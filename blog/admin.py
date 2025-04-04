a9e541aed24a41a01d4d650fc03bd20d2154232c
Farzam Asadian <farzamasadian@gmail.com>
2025-04-04 21:54:11 +0330

    category is added.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,4 +9,6 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status','author')
     search_fields = ['title','content']
 
+
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
