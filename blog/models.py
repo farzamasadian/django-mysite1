d5078370605baa3ecd2e12182649003f4238acde
Farzam Asadian <farzamasadian@gmail.com>
2025-04-04 21:38:27 +0330

    image added to in blog app. author added to the app.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # image 
-    # author
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     # category
     # tage
     counted_views = models.IntegerField(default=0)
