ddb328e7556adef0c2b0a1fc1b9b48500908d92d
Farzam Asadian <farzamasadian@gmail.com>
2025-04-09 20:35:51 +0330

    created some custom tags and implented them in test.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,19 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
 
 
-@register.simple_tag
-def hello():
-    return 'hello'
+@register.simple_tag(name='totalposts')
+def func():
+    posts = Post.objects.filter(status=1)
+    return posts.count()
+
+@register.simple_tag(name='posts')
+def func():
+    posts = Post.objects.filter(status=1)
+    return posts
+
+@register.filter
+def snippet(value, arg=20):
+    return value[:arg]
