bb0e8cc8d1eea79ba65e4e5d1420a14b29c0f9be
Farzam Asadian <farzamasadian@gmail.com>
2025-04-28 17:27:29 +0330

    added multicaptcha to admin log in

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,11 @@ INSTALLED_APPS = [
     'website',
     'blog'
 ]
+
+# captcha admin settings
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 # sites framework
 SITE_ID = 2
