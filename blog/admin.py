f5ba1564f9b4be643ff92f361b77f903e314d746
Farzam Asadian <farzamasadian@gmail.com>
2025-05-03 18:02:30 +0330

    created comment class in blog/models and its options are added within blog/admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 class PostAdmin(SummernoteModelAdmin):
@@ -11,6 +11,13 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title','content']
     summernote_fields = ('content',)
 
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy =  'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name' ,'post' ,'approved','updated_date','created_date')
+    list_filter = ('approved','post')
+    search_fields = ['name','post']
 
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
