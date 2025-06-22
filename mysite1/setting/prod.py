863524d8ebde411cdc096de59ccce912fe4d18cd
Farzam Asadian <farzamasadian@gmail.com>
2025-06-22 12:22:16 +0330

    updated dev and prod settings, also updated manage.py to run the project on dev by default

---
+++ mysite1/setting/prod.py
@@ -0,0 +1,38 @@
+from mysite1.settings import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-g_*t+hb1vkl(gj1gp*yzqz)mzm&b#duxn71uu_*&o&i8e1ql=y'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# sites framework
+SITE_ID = 2
+
+# INSTALLED_APPS
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = 'DENY'
